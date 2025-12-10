63fb284e18aa4303022536aca4ff471ddf6a74f4
rcaposiena-consulting <r.caposiena@gmail.com>
2025-12-10 14:20:27 +0100

    Create liste.py

---
+++ liste.py
@@ -0,0 +1,16 @@
+# ESERCIZIO: Modifica lista numeri
+
+# Crea lista con 5 numeri
+numeri = [10, 20, 30, 40, 50]
+print("Lista iniziale:", numeri)
+
+# Aggiungi un numero alla fine
+numeri.append(60)
+print("Dopo append(60):", numeri)
+
+# Sostituisci il terzo numero (indice 2)
+numeri[2] = 999
+print("Dopo numeri[2] = 999:", numeri)
+
+print("\n--- LISTA FINALE ---")
+print("Lista finale:", numeri)
